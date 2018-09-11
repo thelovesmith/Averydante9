011839d5ecbf339bfde75f1be501bb594ddafc2e
GA Student <gastudent@Generals-MacBook-Air.local>
2018-09-11 09:48:00 -0500

    adds homework

---
+++ homework_part_2/app.js
@@ -0,0 +1,43 @@
+// 1. Interpolation involves assigning a variable with some value or string and using the variable to produce the string or value. 
+// example:
+var name = "john"
+console.log("My Name is " +name);
+
+// Concatenation involves using operators to "add" strings together. This puts the strings together instead of evaluating their total.
+// example:
+console.log("I" + " love" + " to" + " code");
+// Interpolation and concatenation can be used together like the first example.
+
+// 2. DRY stands for "Don't Repeat Yourself". Repeating code is counterproductive and can make your files too large and too populated fo rothers so seamlessly edit it after you. It is good practice to use tools like for and while loops to avoid repeating code. We want effiecient and useful code. 
+// 3. Declaring a variable means your prety much letting the computer know that this "variable" exists. It slike a placeholder. Once you assign a value to it then it can be used in formulas and/or functions. Defining a variable pretty much means your giving it a value of some kind. 
+// example
+var debt;
+// declaring a variable
+var money = 500;
+var houseSize = "Big";
+// defining variable 
+// 4.
+// 5. A parent directory is the directory that comes before the directory we are accessing at the moment. Whixh ever directory you are in, the parent directory is the one that encompasses that. 
+// 6. To get more info on a Unix commmand, you can enter "man" before the command  in the command line
+// example; ( man cd) or (man mkdir)
+// 7. Tab completion is a shortcut key that lets us choose directory or  files when running commands in the command line interface. After you enter a command you type the first letter of th efile or directory and then 'tab' and the file or files with that first letter will come up. 
+ // PART 2
+
+const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+ a < b;
+ c > d;
+ "name" == "name";
+ a !== b !== c;
+a === a < d;
+e == 'Kevin';
+48 !== '48';
+
+// Part 3
+
+// 1. Yes, this code is an infinite loop because th ecode will continue to run "while" true is true. And treu continuously is true because there is no code present to "break" or stop the loop.
+2. 
